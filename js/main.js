9cf41721db5f3942569d0893880d1de130c7d9a8
Abel Arismendy <arismendyc.design@gmail.com>
2020-05-07 20:50:17 -0500

    Challenge #2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 const d = document;
+
 const helloWorldInput = d.getElementById('helloWorldInput');
 const helloWorldDone = d.getElementById('helloWorldDone');
 const helloWorldResult = d.getElementById('helloWorldResult');
+
+const secondDone = d.getElementById('secondDone');
+const secondResult = d.getElementById('secondResult');
+const nameInput = d.getElementById('nameInput');
+const surnameInput = d.getElementById('surnameInput');
 
 const helloWorld = () => {
     helloWorldDone.addEventListener('click', ()=> {
         helloWorldResult.innerHTML = 'Hola, ' + helloWorldInput.value;
     });
-}
+};
 
 helloWorld();
+
+const secondChallenge = () => {
+    secondDone.addEventListener('click', ()=> {
+        secondResult.innerHTML = 'Hola, ' + nameInput.value + ' ' + surnameInput.value;
+    });
+};
+
+secondChallenge();
