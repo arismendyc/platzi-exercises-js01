421f45dbb7e49bae6d9257575f280a1bfc79859e
Abel Arismendy <arismendyc.design@gmail.com>
2020-05-07 21:14:30 -0500

    Challenge #3 and improve #2, #3

--- js/main.js
+++ js/main.js
@@ -1,26 +1,47 @@
 const d = document;
 
+//first challenge
+
 const helloWorldInput = d.getElementById('helloWorldInput');
 const helloWorldDone = d.getElementById('helloWorldDone');
 const helloWorldResult = d.getElementById('helloWorldResult');
+
+const helloWorld = () => {
+    helloWorldDone.addEventListener('click', ()=> {
+        helloWorldResult.innerHTML = `Hola, ${helloWorldInput.value}`;
+    });
+};
+
+helloWorld();
+
+//const challenge
 
 const secondDone = d.getElementById('secondDone');
 const secondResult = d.getElementById('secondResult');
 const nameInput = d.getElementById('nameInput');
 const surnameInput = d.getElementById('surnameInput');
 
-const helloWorld = () => {
-    helloWorldDone.addEventListener('click', ()=> {
-        helloWorldResult.innerHTML = 'Hola, ' + helloWorldInput.value;
-    });
-};
-
-helloWorld();
-
 const secondChallenge = () => {
     secondDone.addEventListener('click', ()=> {
-        secondResult.innerHTML = 'Hola, ' + nameInput.value + ' ' + surnameInput.value;
+        secondResult.innerHTML = `Hola, ${nameInput.value} ${surnameInput.value}`;
     });
 };
 
 secondChallenge();
+
+// third challenge
+
+const thirdResult = d.getElementById('thirdResult');
+var coursesCategories = ["Developing","Design","Marketing","Business","Audiovisual production","Profesional development"];
+
+const thirdChallenge = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        let categorie = `<li>${array[i]}</li>`
+        thirdResult.insertAdjacentHTML('beforeend', categorie);
+    }
+}
+
+thirdChallenge(coursesCategories);
+
+//fourth challenge
+
